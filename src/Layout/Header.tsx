f362f1a4877afbfaaa0612d580a94da89dc11f20
Timi-Leyin <timileyinoyelekan11@gmail.com>
2022-09-08 18:34:14 +0100

    fix: deploy error

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,10 +1,9 @@
-import logo from '../Assets/logo.png';
 
 const Header = () => {
   return (
     <header className='p-4'>
         <div className="logo">
-          <img src={logo} alt="" className="w-[60px] mx-auto" />
+          <img src={'/logo.png'} alt="" className="w-[60px] mx-auto" />
         </div>
         
         {/* <div className="hamburger"></div> */}
