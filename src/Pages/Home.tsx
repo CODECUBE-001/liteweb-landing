13a133dc391240a9d2e0bb4ee6c4f070e1d0f1b1
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-09-08 22:41:32 -0700

    Added image to list items

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -58,13 +58,20 @@ const Home = () => {
       <section className="my-12 py-12 px-4">
         <div className='flex flex-col md:flex-row  relative py-12  justify-around'>
           {/* @comp */}
-          <div className="max-w-[400px] my-5 md:my-0">
+          <div className="max-w-[400px] my-5 md:my-0 listo">
             <h3 className="text-5xl">
               Built for the way you work
             </h3>
             <p className='text-sm py-4'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae veritatis non earum error temporibus accusamus.
             </p>
+              <li>Drag and Drop Website Builder</li>
+              <li>400+ pre made blocks</li>
+              <li>Export Your code to your Device</li>
+              <li>Free Subdomain</li>
+              <li>Add Your own Domain (Coming Soon)</li>
+              <li>Push to Cpanel Directly</li>
+              <li>Add Your own code</li>
             <div className="flex gap-3 text-xs items-center">
               <span className='text-primary underline'>Keep Scrolling</span>
               <span><ArrowDown /></span>
