cd0fca19a47adb02761445211812d5d87288b57d
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-11-15 16:16:43 +0100

    Build added

--- src/Components/index.ts
+++
@@ -1,8 +0,0 @@
-export {default as Hero}from './hero/Hero'
-export {default as Features}from './features/Features'
-export {default as Showcase}from './showcase/showcase'
-export {default as Pricing}from './pricing/Pricing'
-export {default as Blog} from './blog/Blog'
-export {default as Cta} from './cta/Cta'
-export {default as Faq} from './faq/Faq'
-export { default as Chatbot } from "./chatbot/Chatbot";
--- src/Layout/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as Footer} from './footer/Footer'
-export {default as Header} from './Header/Header'
-export {default as Body} from './body/Body'
